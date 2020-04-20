1ef31321eb1566805622653543f50d14dcb65800
Andrew Adcock <aadcock@collabralink.com>
2020-04-20 11:05:58 -0400

    Update pm run commands to start both servers concurrently

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Don't you date edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
